d4369c9404ddcb928c80f959d6deff4a4f4ddbb4
nitusima <nkataraia@d-tree.org>
2024-12-14 12:43:55 +0300

    .

--- code/dihlibs/db.py
+++ code/dihlibs/db.py
@@ -134,7 +134,7 @@ class DB:
         elif pd.api.types.is_datetime64_any_dtype(dtype):
             return f"'{value.strftime('%Y-%m-%d %H:%M:%S')}'::TIMESTAMP"
         elif isinstance(value,(list,dict)):
-            return f"'{json.dumps(value).replace("'","''")}'::JSONB"
+            return f"""'{json.dumps(value).replace("'","''")}'::JSONB"""
         else:  # Default to string
             v=str(value).replace("'", "''");
             return f"'{v}'"
