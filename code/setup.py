234a969a3e9f92db47afd5032eb78aee6cef45c1
Nitu <nkataraia@gmail.com>
2025-07-03 16:43:51 +0300

    updated the version number

--- code/setup.py
+++ code/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dihlibs",
-    version="0.0.76",
+    version="0.0.77",
     author="Nitu",
     author_email="nkataraia@d-tree.org",
     description="A helper package for data integrations",
